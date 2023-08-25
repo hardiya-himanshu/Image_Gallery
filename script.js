52f0d543b49c02c8b8843b3e191780cbaa7a2f5b
Himanshu Hardiya <himanshuhardiya18@gmail.com>
2023-08-25 22:13:30 +0530

    successfully created image gallery

---
+++ script.js
@@ -0,0 +1,19 @@
+let scrollContainer = document.querySelector(".gallery");
+let prevBtn = document.getElementById("prevBtn");
+let nextBtn = document.getElementById("nextBtn");
+
+scrollContainer.addEventListener("wheel", (e)=>{
+    e.preventDefault();
+    scrollContainer.scrollLeft+=3*(e.deltaY);
+    scrollContainer.style.scrollBehavior = "smooth";
+
+})
+
+prevBtn.addEventListener("click", ()=>{
+    scrollContainer.scrollLeft-=340;
+})
+
+nextBtn.addEventListener("click", ()=>{
+    scrollContainer.scrollLeft+=340;
+})
+
